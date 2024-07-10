4c80583c245c8bdc0a6f7ef53ca4e6927c1d634f
IriMerkulova <151544565+IriMerkulova@users.noreply.github.com>
2024-07-11 02:30:35 +0300

    Add files via upload

---
+++ exempl01/Program.cs
@@ -0,0 +1,20 @@
+﻿//Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
+//Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. Использовать исключительно массивы.
+
+Console.WriteLine ("Введите строки массива через пробел!");
+
+String M = Console.ReadLine();
+
+String[] mases = M.Split(' ');
+var massiv = new String[mases.Length];
+var i=0;
+
+foreach (var mas in mases)
+{
+     if (mas.Length <= 3)
+    {
+        massiv[i] = mas;
+        i++;
+    }
+}
+Console.WriteLine ("{0}", String.Join(" ", massiv));
